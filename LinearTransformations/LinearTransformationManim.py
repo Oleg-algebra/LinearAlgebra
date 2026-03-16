bbfe7e2891a73e22ed6487e0ede64be5021ee7ae
OlegTyshchenko <sokol.wise@gmail.com>
2026-03-16 20:58:14 +0200

    LinearTransformationManim.py

---
+++ LinearTransformations/LinearTransformationManim.py
@@ -0,0 +1,62 @@
+from manim import *
+import numpy as np
+
+
+class LinearTransformation(Scene):
+    def construct(self):
+        # 1. Load Matrix
+        try:
+            matrix = np.loadtxt("input.txt")
+        except:
+            # A shear + scale that would usually show edges
+            matrix = [[2, 1], [0.5, 1]]
+
+        # 2. Create an Oversized Background Grid (Static)
+        background_plane = NumberPlane(
+            x_range=[-20, 20, 1],  # Much wider than the screen
+            y_range=[-20, 20, 1],  # Much taller than the screen
+            background_line_style={
+                "stroke_color": YELLOW,
+                "stroke_width": 4,
+                "stroke_opacity": 0.2
+            }
+        )
+
+        # 3. Create an Oversized Moving Grid (High Visibility)
+        moving_plane = NumberPlane(
+            x_range=[-20, 20, 1],
+            y_range=[-20, 20, 1],
+            background_line_style={
+                "stroke_color": BLUE,
+                "stroke_width": 6,
+                "stroke_opacity": 0.6
+            }
+        )
+
+        # Add coordinates only to the center area so they don't clutter
+        background_plane.add_coordinates(range(-10, 11),range(-10, 11))
+
+        # 4. The "Unit Square" (To show Area Change)
+        # This square will transform along with the grid
+        unit_square = Square(side_length=1, fill_color=YELLOW, fill_opacity=0.3, stroke_width=2)
+        unit_square.move_to(moving_plane.coords_to_point(0.5, 0.5))
+
+        # 4. Basis Vectors
+        i_hat = Vector([1, 0], color=YELLOW).set_stroke(width=8)
+        j_hat = Vector([0, 1], color=PINK).set_stroke(width=8)
+
+        # 5. Animation
+        # 6. Animation Logic
+        self.add(background_plane, moving_plane, unit_square, i_hat, j_hat)
+
+        # We use a slightly zoomed out camera or just keep the default
+        # The 'fading' edges effect can be ignored because the grid is so large
+        self.play(
+            moving_plane.animate.apply_matrix(matrix),
+            i_hat.animate.apply_matrix(matrix),
+            j_hat.animate.apply_matrix(matrix),
+            unit_square.animate.apply_matrix(matrix),
+            run_time=4,
+            rate_func=smooth
+        )
+        self.wait(2)
